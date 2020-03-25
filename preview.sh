a529b7f13b678eb9d205291debda92ae8d17713b
Sean Anastasi <sean@anasta.si>
2020-03-24 22:40:46 -0700

    Start adding some more preview options
    Some of these still crash the computer for some reason...

--- preview.sh
+++ preview.sh
@@ -2,10 +2,32 @@
 #
 # Preview the currently installed Plymouth theme
 #
+set -eu
 
 DURATION="${1:-5}"
+COMMAND="${2:-wait}"
 
 plymouthd
+
+function quit {
+    plymouth quit
+}
+trap quit EXIT
+
 plymouth --show-splash
-sleep "$DURATION"
-plymouth quit
+
+case $COMMAND in
+wait)
+    sleep $DURATION
+    ;;
+status)
+    for i in $(seq 1 $DURATION); do
+        plymouth --update=$(date)
+        sleep 1
+    done
+    ;;
+password)
+    plymouth --ask-for-password
+    sleep $DURATION
+    ;;
+esac
