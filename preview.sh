aafd059c770ac096469ef7135c0ae59c64fc6eee
Sean Anastasi <sean@anasta.si>
2020-03-25 21:40:00 -0700

    Add new preview functions for status and message

--- preview.sh
+++ preview.sh
@@ -7,14 +7,21 @@ set -eu
 DURATION="${1:-5}"
 COMMAND="${2:-wait}"
 
-plymouthd
+plymouthd --debug --debug-file=/home/sanasta/documents/plymouthd.log
 
 function quit {
+    echo "Quitting"
     plymouth quit
 }
 trap quit EXIT
 
-plymouth --show-splash
+# Wait for plymouth to actually be available or else we'll end up switching to a
+# new TTY without showing plymouth for some reason.
+while ! plymouth --ping; do
+    sleep 1
+done
+
+plymouth show-splash
 
 case $COMMAND in
 wait)
@@ -22,12 +29,25 @@ wait)
     ;;
 status)
     for i in $(seq 1 $DURATION); do
-        plymouth --update=$(date)
-        sleep 1
+        message="$(date) $i"
+        echo "update: $message"
+        plymouth update --status="$message"
+        sleep 0.5
+    done
+    ;;
+message)
+    for i in $(seq 1 $DURATION); do
+        message="$(date) $i"
+        echo "display-message: $message"
+        plymouth display-message --text="$message"
+        sleep 0.5
     done
     ;;
 password)
-    plymouth --ask-for-password
+    plymouth ask-for-password -command=echo --prompt=Password
     sleep $DURATION
     ;;
 esac
+
+echo "EOF"
+sleep 1
